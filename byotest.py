9edffa0b871745f0820b93115e66ee096e9888c7
Aileen Donegan <aileendonegan@Aileens-MacBook-Air.local>
2019-09-25 13:30:12 +0100

    trying out a positive return statement

--- byotest.py
+++ byotest.py
@@ -8,7 +8,9 @@ def test_not_equal(a, b):
     assert a != b, "Did not expect {0} but got {1}".format(a, b) 
 
 def test_is_in(collection, item):
-    assert item in collection, "{0} does not contain {1}".format(collection, item)       
+    assert item in collection, "{0} does not contain {1}".format(collection, item)
+    if item in collection:
+        return "passed"       
 
 # test_are_equal(number_of_evens([1,2,3,4,5]), 2)
 # test_not_equal(0,0)
