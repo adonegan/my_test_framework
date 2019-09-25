8ca41113588ec72187b947c56b107cf6697a7429
Aileen Donegan <aileendonegan@Aileens-MacBook-Air.local>
2019-09-25 13:09:29 +0100

    called testing function test_are_equal

--- byotest.py
+++ byotest.py
@@ -3,3 +3,5 @@ def number_of_evens(numbers):
 
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+
+test_are_equal(number_of_evens([1,2,3,4,5]), 2)
