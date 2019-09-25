b947222c95415a090f66c1604cdc6079066f8997
Aileen Donegan <aileendonegan@Aileens-MacBook-Air.local>
2019-09-25 13:55:21 +0100

    added test_between function

--- byotest.py
+++ byotest.py
@@ -11,9 +11,12 @@ def test_is_in(collection, item):
     assert item in collection, "{0} does not contain {1}".format(collection, item) 
 
 def test_not_in(collection, item):
-    assert not item in collection, "{0} is in {1}".format(collection, item)    
+    assert not item in collection, "{0} is in {1}".format(collection, item) 
+
+def test_between(x, y):
+    assert x <= y, "This number is between {0} and {1}".format(x, y)        
 
 # test_are_equal(number_of_evens([1,2,3,4,5]), 2)
 # test_not_equal(0,0)
 # test_is_in([0], 2)
-test_not_in([1], 2)
+# test_not_in([1], 2)
